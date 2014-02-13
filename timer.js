9651c894f62dd05d42b18243ca890f30b4f7ffde
Schusswill <William.schussler2014@gmail.com>
2014-02-13 17:10:10 -0600

    stuff

---
+++ timer.js
@@ -0,0 +1,11 @@
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     return;
+  }
+
+ document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
+}
